1f5dd4575e6e30504de27a2ec6b8bea1024183ff
tincho <martin.salinas@gmail.com>
2021-05-20 09:03:46 -0300

    rebuild

---
+++ reminders-calendar/precache-manifest.4569d8643973af25399891cc7b7227e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40e6544a4ebceef3af0a9df3f3cb070",
+    "url": "./index.html"
+  },
+  {
+    "revision": "49f6189f776ae8930392",
+    "url": "./static/css/main.ce895159.chunk.css"
+  },
+  {
+    "revision": "90808221c8468b1f0a60",
+    "url": "./static/js/2.66afd367.chunk.js"
+  },
+  {
+    "revision": "cb4293a028d0175415ddb31dc1aaaefb",
+    "url": "./static/js/2.66afd367.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49f6189f776ae8930392",
+    "url": "./static/js/main.f195fb85.chunk.js"
+  },
+  {
+    "revision": "f08517a9af3815173457",
+    "url": "./static/js/runtime-main.b82373d0.js"
+  }
+]);
